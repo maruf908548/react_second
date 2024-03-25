4bf5d1ddc984fc150224c332d541abb40ba440b7
maruf908548 <ahmedmaruf908548@gmail.com>
2024-03-26 01:18:21 +0600

    import BlockCerts

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useRoutes } from "react-router-dom";
 import Home from "pages/BlockCerts/Home";
 import NotFound from "pages/NotFound";
-import WwwblockcertsorgbyhtmltodesignFREEversion25Zero from "pages/BlockCerts/Home";
+import BlockCerts from "pages/BlockCerts/Home";
 
 const ProjectRoutes = () => {
   let element = useRoutes([
